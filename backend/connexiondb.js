2fc4256c393280d5e443470d53e073b2558860b2
fabrice34 <labarrefabrice6@gmail.com>
2020-12-10 07:20:43 +0100

    backend groupomania modif app.js et debut frontend groupomania

--- backend/connexiondb.js
+++ backend/connexiondb.js
@@ -2,7 +2,7 @@ const dotenv = require ('dotenv') . config ( )
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASS, { dialect: "mysql",  host: process.env.DB_HOST });
 
-const dbTest = async function () {
+const db = async function () {
     try {
       await sequelize.authenticate();
       console.log('Connexion réussi !');
@@ -10,6 +10,6 @@ const dbTest = async function () {
       console.error('Connexion échouée !', error);
     }
   };
-  dbTest();
+  db();
 
 module.exports= sequelize
